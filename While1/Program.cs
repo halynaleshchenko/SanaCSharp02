35e54b36370767665900c4eeb545e9a4799109f1
halynaleshchenko <ipz222_lgo@student.ztu.edu.ua>
2022-12-27 17:32:47 +0200

    +While..

---
+++ While1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("n="); byte n = byte.Parse(Console.ReadLine());
+double a=0, b=0, c=0; byte m=1;
+while (m<=n){
+    a += (Math.Pow((-1), m)) / (2 * m + 1);
+    b += 1 + (1) / (Math.Pow(m, 2));
+    int factor=1;
+    for(byte i = 0; i<m; i++)
+    {
+       factor*=m - i;
+    }
+    c += factor;
+    m++;
+    
+}
+Console.WriteLine("a:"+a);
+Console.WriteLine("b:"+b);
+Console.WriteLine("c:"+c);
+
